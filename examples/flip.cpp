1e65f16ecc9270263dacc7aaeabb9063ade75d6a
Bobby-0228 <k970244.ee09@nycu.edu.tw>
2026-03-15 21:01:00 +0800

    add example: flip

---
+++ examples/flip.cpp
@@ -0,0 +1,15 @@
+#include "bmp.h"
+#include "basic.h"
+#include "ops.h"
+
+int main()
+{
+    BMP bmp("images/inputs/normal/mountain.bmp");
+
+    BMP flipped_horizontally = flip_horizontally(bmp);
+    flipped_horizontally.write("images/outputs/mountain_flip_hor.bmp");
+
+    BMP flipped_vertically = flip_vertically(bmp);
+    flipped_vertically.write("images/outputs/mountain_flip_ver.bmp");
+    return 0;
+}
